6cf37d15404924e099b0a06c2ad49118dc68e01e
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-20 14:46:53 +0800

    Format

--- src/Main.java
+++ src/Main.java
@@ -11,78 +11,66 @@ public class Main {
 		
 		List<Subject> listSubjects = new ArrayList<Subject>();
 		
-	while(true) {
-		try {
-			System.out.println("[1] Add New Subject");
-			System.out.println("[2] Search Subject");
-			System.out.println("[3] Modify the details of the Subject");
-			System.out.println("[4] Remove a Subject");
-			System.out.println("[5] Display Subject");
-			
-			System.out.println("\nEnter your Command: ");
-			int command =  Integer.parseInt(rd.readLine());
-			
-			switch(command) {
-			
-			case 1:
-				try {
-					System.out.print("Enter Subject Name: ");
-					String name = br.readLine();
-					System.out.print("Enter Number of Units: ");
-					int units = Integer.parseInt(rd.readLine());
-					listSubjects.add(new Subject(name, units));
-				} catch (Exception e) {
-					System.out.println("Invalid Input.\n");
-				}
-				break;
-			case 2:
-				System.out.print("Enter the ID of the subject you're looking for: ");
-				int id = Integer.parseInt(br.readLine());
-				for(int i = 0; i<listSubjects.size();i++) {
-					if(listSubjects.get(i).getSubjectId() == id) {
-						System.out.println("Subject found!");
-						listSubjects.get(i).Display();
-						break;
-					}else {
-						System.out.println("Subject not found.");
+		while(true) {
+			try {
+				System.out.println("[1] Add New Subject");
+				System.out.println("[2] Search Subject");
+				System.out.println("[3] Modify the details of the Subject");
+				System.out.println("[4] Remove a Subject");
+				System.out.println("[5] Display Subject");
+				
+				System.out.println("\nEnter your Command: ");
+				int command =  Integer.parseInt(br.readLine());
+				
+				switch(command) {
+				
+				case 1:
+					try {
+						System.out.print("Enter Subject Name: ");
+						String name = br.readLine();
+						System.out.print("Enter Number of Units: ");
+						int units = Integer.parseInt(br.readLine());
+						listSubjects.add(new Subject(name, units));
+					} catch (Exception e) {
+						System.out.println("Invalid Input.\n");
 					}
+					break;
+				case 2:
+					System.out.print("Enter the ID of the subject you're looking for: ");
+					int id = Integer.parseInt(br.readLine());
+					for(int i = 0; i<listSubjects.size();i++) {
+						if(listSubjects.get(i).getSubjectId() == id) {
+							System.out.println("Subject found!");
+							listSubjects.get(i).Display();
+							break;
+						}else {
+							System.out.println("Subject not found.");
+						}
+					}
+					break;
+				case 3:
+					break;
+				case 4:
+					System.out.println("Enter the Subjects ID: ");
+					id = Integer.parseInt(br.readLine());
+					for (int index = 0; index < listSubjects.size(); index++) {
+						if (listSubjects.get(index).getSubjectId() == id) {
+
+							listSubjects.remove(index);
+						}
+					}
+					break;
+				case 5:
+					System.out.println("Subject Details: \n");
+					for(int i = 0; i<listSubjects.size();i++) {
+						listSubjects.get(i).Display();
+					}
+					break;
 				}
-				break;
-			case 3:
-				break;
-			case 4:
-				System.out.println("Enter the Subjects ID: ");
-
-					int id = Integer.parseInt(br.readLine(System.in));
-						for (int index = 0; index < listSubject.size(); index++) {
-							if (listSubject.get(index).getSubjectID() == id) {
-
-								listSubject.remove(index);
+				
+			} catch (Exception e) {
+				System.out.println("Invalid Input.\n");
 			}
 		}
-				break;
-			case 5:
-				System.out.println("Subject Details: \n");
-				for(int i = 0; i<listSubjects.size();i++) {
-					listSubjects.get(i).Display();
-				}
-				break;
-			}
-			
-		} catch (Exception e) {
-			System.out.println("Invalid Input.\n");
-		}
 	}
-			
-		
-		
-		
-		
-		
-		
-		
-	
-
-	}
-
 }
