eb17cc1b9a41ea5d06674dc792e4b2ca96bd9de5
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-20 14:14:23 +0800

    Reformat

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,34 @@ public class Main {
 		
 		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
 		
-		List<Subject> listSubject = new ArrayList<Subject>();
+		List<Subject> listSubjects = new ArrayList<Subject>();
 		
-		listSubject.add(new Subject(1, "OOP",3));
-		listSubject.add(new Subject(2, "DSA",3));
-		listSubject.add(new Subject(3, "DS",3));
-		listSubject.add(new Subject(4, "HIS",3));
+	while(true) {
+		try {
+			System.out.println("[1] Add New Subject");
+			System.out.println("[2] Search Subject");
+			System.out.println("[3] Modify the details of the Subject");
+			System.out.println("[4] Remove a Subject");
+			System.out.println("[5] Display Subject");
 			
-		System.out.println("[1] Add New Subject");
-		System.out.println("[2] Search Subject");
-		System.out.println("[3] Modify the details of the Subject");
-		System.out.println("[4] Remove a Subject");
-		System.out.println("[5] Display Subject");
-		
+			while(true) {
+				try {
+					System.out.println("\nEnter your Command: ");
+					int command =  Integer.parseInt(rd.readLine());
+					
+					switch(command) {
+					
+					}
+				} catch (Exception e) {
+					System.out.println("Invalid Input.");
+				}
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Invalid Input.");
+		}
+	}
+			
 		
 		
 		
