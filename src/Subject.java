f2f4e8cb7776e70a15201ee01ea232471871f0f5
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-20 15:10:27 +0800

    Comments

--- src/Subject.java
+++ src/Subject.java
@@ -5,6 +5,10 @@ public class Subject {
 	private String subjectName;
 	private int noOfUnits;
 	
+	/*
+	 * Ths static int ID will be increased on each creation of a subject.
+	 * That id will be assigned on the created subject.
+	 */
 	public Subject( String subjectName, int noOfUnits) {
 		Subject.ID++;
 		this.subjectId = ID;
@@ -23,7 +27,7 @@ public class Subject {
 	public void setNoOfUnits(int noOfUnits) {
 		this.noOfUnits = noOfUnits;
 	}
-	
+	//displays the informations of the subject.
 	public void Display() {
 		System.out.println("Subject ID: "+this.subjectId);
 		System.out.println("Subject Name: "+this.subjectName);
