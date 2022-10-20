4366317cc4ac15ed6d7db1de652184598c5edfc0
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-20 14:25:06 +0800

    Add Subject

--- src/Main.java
+++ src/Main.java
@@ -19,33 +19,38 @@ public class Main {
 			System.out.println("[4] Remove a Subject");
 			System.out.println("[5] Display Subject");
 			
-			while(true) {
+			System.out.println("\nEnter your Command: ");
+			int command =  Integer.parseInt(rd.readLine());
+			
+			switch(command) {
+			
+			case 1:
 				try {
-					System.out.println("\nEnter your Command: ");
-					int command =  Integer.parseInt(rd.readLine());
-					
-					switch(command) {
-					
-					case 1:
-					break;
-					case 2:
-					break;
-					case 3:
-					break;
-					case 4:
-					break;
-					case 5:
-					break;
-					
-						
-					}
+					System.out.print("Enter Subject Name: ");
+					String name = rd.readLine();
+					System.out.print("Enter Number of Units: ");
+					int units = Integer.parseInt(rd.readLine());
+					listSubjects.add(new Subject(name, units));
 				} catch (Exception e) {
-					System.out.println("Invalid Input.");
+					System.out.println("Invalid Input.\n");
 				}
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				System.out.println("Subject Details: \n");
+				for(int i = 0; i<listSubjects.size();i++) {
+					listSubjects.get(i).Display();
+				}
+				break;
 			}
 			
 		} catch (Exception e) {
-			System.out.println("Invalid Input.");
+			System.out.println("Invalid Input.\n");
 		}
 	}
 			
