2566f1cba2578732fc07c2fdb8f01b5712b4cfc9
SSLayZ <115135481+SSLayZ@users.noreply.github.com>
2022-10-20 14:04:51 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+		
+		List<Subject> listSubject = new ArrayList<Subject>();
+		
+		listSubject.add(new Subject(1, "OOP",3));
+		listSubject.add(new Subject(2, "DSA",3));
+		listSubject.add(new Subject(3, "DS",3));
+		listSubject.add(new Subject(4, "HIS",3));
+			
+		System.out.println("[1] Add New Subject");
+		System.out.println("[2] Search Subject");
+		System.out.println("[3] Modify the details of the Subject");
+		System.out.println("[4] Remove a Subject");
+		System.out.println("[5] Display Subject");
+		
+		
+		
+		
+		
+		
+		
+		
+	
 
 	}
 
