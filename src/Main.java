8220274d5d801729cf5667c1fb88077c2f851738
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-10-20 14:15:38 +0800

    cases added

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,18 @@ public class Main {
 					
 					switch(command) {
 					
+					case 1:
+					break;
+					case 2:
+					break;
+					case 3:
+					break;
+					case 4:
+					break;
+					case 5:
+					break;
+					
+						
 					}
 				} catch (Exception e) {
 					System.out.println("Invalid Input.");
